2521af82616027608eacb26448bdd1c44f48c752
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-07-29 19:02:42 +0900

    ref

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -2,48 +2,35 @@ import { Todo } from "@/interfaces/todos";
 
 const baseUrl = "http://localhost:3001";
 
-export const readTodosApi = async (): Promise<Todo[]> => {
-  const res = await fetch(`${baseUrl}/tasks`, { cache: "no-store" });
-  const todos = await res.json();
+const requestOptions = (method: string, body?: object) => ({
+  method,
+  headers: { "Content-Type": "application/json" },
+  body: body ? JSON.stringify(body) : null,
+});
 
-  return todos;
+export const readTodosApi = async (): Promise<Todo[]> => {
+  // SSR
+  const res = await fetch(`${baseUrl}/tasks`, { cache: "no-store" });
+  return await res.json();
 };
 
 export const createTodoApi = async (todo: Todo): Promise<Todo> => {
-  const res = await fetch(`${baseUrl}/tasks`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(todo),
-  });
-  const newTodo = await res.json();
-  return newTodo;
+  const res = await fetch(`${baseUrl}/tasks`, requestOptions("POST", todo));
+  return await res.json();
 };
 
 export const updateTodoApi = async (
   id: string,
   newText: string
 ): Promise<Todo> => {
-  const res = await fetch(`${baseUrl}/tasks/${id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ text: newText }),
-  });
-  const updatedTodo = await res.json();
-
-  return updatedTodo;
+  const res = await fetch(
+    `${baseUrl}/tasks/${id}`,
+    requestOptions("PUT", { text: newText })
+  );
+  return await res.json();
 };
 
 export const deleteTodoApi = async (id: string): Promise<Todo> => {
-  const res = await fetch(`${baseUrl}/tasks/${id}`, {
-    method: "DELETE",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-  const deleteTodoApi = await res.json();
-  return deleteTodoApi;
+  const res = await fetch(`${baseUrl}/tasks/${id}`, requestOptions("DELETE"));
+  return await res.json();
 };
